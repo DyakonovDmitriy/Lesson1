b6c2b1ea3cbc2ed8757104e8fbdcc4c90ffa2492
Dmitriy <dmitriy_logvinenko@yahoo.com>
2020-03-04 23:07:48 +0300

    Кое что создал

--- Main.py
+++ Main.py
@@ -1,7 +1,2 @@
-import requests
-import json
-
-url = 'http://wwww.cbr-xml-daily.ru/daily_json.js'
-respond = requests.get(url)
-data = json.loads(respond.text)
-print(data)
+t = "Hello World!!!"
+print(t)
